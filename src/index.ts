05455cfc65751d2d3953dca52391b5436a3143b3
denismaster <denismaster@outlook.com>
2018-05-22 08:24:46 +0300

    Add code to test lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from './greeter';
+import greeter from './greeter';
+
+const latex = `
+    \\documentclass[a4paper]{article}
+    \\begin{document}
+    Hello world!
+    \\end{document}
+`;
+
+greeter(latex, {});
